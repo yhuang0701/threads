51bd7243cace3e0cd55a25e5656b78f896e63580
Yichen Huang <yichen41@illinois.edu>
2024-06-10 14:23:50 -0500

    Clerk Imports - SignIn & SignUp Page

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <main>
+      <h1 className="head-text">
+        Onboarding Page
+      </h1>
+    </main>
+  )
+}
+export default page
---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+
+import "./globals.css";
+
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 14 Meta Threads Application"
+}
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({
+  children
+}: {
+  children: ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider >
+  )
+
+}
